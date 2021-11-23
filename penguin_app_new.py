1338567423fded20b3ed4f64b3a8778ee087356d
Manreet Grewal <mank108.g@gmail.com>
2021-11-23 22:05:21 +0530

    Update penguin_app_new.py

--- penguin_app_new.py
+++ penguin_app_new.py
@@ -1,5 +1,3 @@
-# Open Sublime text editor, create a new Python file, copy the following code in it and save it as 'penguin_app.py'.
-
 # Importing the necessary libraries.
 import numpy as np
 import pandas as pd
@@ -13,7 +11,7 @@ from sklearn.ensemble import RandomForestClassifier
 
 # Load the DataFrame
 csv_file = 'penguin.csv'
-df = pd.read_csv(csv_file)
+df = pd.read_csv("penguin.csv")
 
 # Display the first five rows of the DataFrame
 df.head()
@@ -54,3 +52,67 @@ log_reg_score = log_reg.score(X_train, y_train)
 rf_clf = RandomForestClassifier(n_jobs = -1)
 rf_clf.fit(X_train, y_train)
 rf_clf_score = rf_clf.score(X_train, y_train)
+
+
+# Create a function that accepts 'island', 'bill_length_mm', 'bill_depth_mm', 'flipper_length_mm', 'body_mass_g' and 'sex' as inputs and returns the species name.
+@st.cache()
+def prediction(model, island, bill_length_mm, bill_depth_mm, flipper_length_mm, body_mass_g, sex):
+  species = model.predict([[island, bill_length_mm, bill_depth_mm, flipper_length_mm, body_mass_g, sex]])
+  species = species[0]
+  #'Adelie': 0, 'Chinstrap': 1, 'Gentoo':2
+  if species == 0:
+    return "Adelie"
+  elif species == 1:
+    return "Chinstrap"
+  else:
+    return "Gentoo"  
+
+# Design the App
+
+# Add title widget
+st.title("Penguin Species Prediction App")  
+
+# Add 4 sliders for numeric values  - 'bill_length_mm', 'bill_depth_mm', 'flipper_length_mm', 'body_mass_g' and store the value returned by them in 5 separate variables.
+b_len = st.slider("Bill Length in mm", float(df['bill_length_mm'].min()), float(df['bill_length_mm'].max()))
+b_dep = st.slider("Bill Depth in mm", float(df['bill_depth_mm'].min()), float(df['bill_depth_mm'].max()))
+f_len = st.slider("Flipper Length in mm", float(df['flipper_length_mm'].min()), float(df['flipper_length_mm'].max()))
+body_mass = st.slider("Body Mass in gms", float(df['body_mass_g'].min()), float(df['body_mass_g'].max()))
+
+# Add 2 dropdown for categorical values  - 'sex' and 'island' and convert them to numeric
+gen = st.selectbox('Gender', ('Male', 'Female'))
+
+if gen == 'Male':
+  gen = 0
+else:
+  gen = 1
+
+
+isl = st.selectbox('Island', ('Biscoe', 'Dream', 'Torgersen'))
+if isl == 'Biscoe':
+  isl = 0
+elif isl == 'Dream':
+  isl = 1
+else:
+  isl = 2  
+
+
+classifier = st.sidebar.selectbox('Classifier', ('Support Vector Machine', 'Logistic Regression', 'Random Forest Classifier'))
+
+# When the 'Predict' button is clicked, check which classifier is chosen and call the 'prediction()' function.
+# Store the predicted value in the 'species_type' variable accuracy score of the model in the 'score' variable. 
+# Print the values of 'species_type' and 'score' variables using the 'st.text()' function.
+if st.sidebar.button("Predict"):
+  if classifier == 'Support Vector Machine':
+    species_type = prediction(svc_model, isl, b_len, b_dep, f_len, body_mass, gen)
+    score = svc_score
+
+  elif classifier =='Logistic Regression':
+    species_type = prediction(log_reg, isl, b_len, b_dep, f_len, body_mass, gen)
+    score = log_reg_score
+
+  else:
+    species_type = prediction(rf_clf, isl, b_len, b_dep, f_len, body_mass, gen)
+    score = rf_clf_score
+  
+  st.write("Species predicted:", species_type)
+  st.write("Accuracy score of this model is:", score)
